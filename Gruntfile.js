d5b6ac07f597fc59cd1c058bb0a0abd53a101151
sarahkasiske <s.kasiske@umiami.edu>
2015-11-02 19:15:03 -0500

    first Commit

--- Gruntfile.js
+++ Gruntfile.js
@@ -181,7 +181,7 @@ module.exports = function (grunt) {
                 options: {
                     watchTask: true,
                     // fill in proxy address of local WP server
-                    proxy: ""
+                    proxy: "http://sarahkasiske.com:8888/"
                 }
             }
         }
